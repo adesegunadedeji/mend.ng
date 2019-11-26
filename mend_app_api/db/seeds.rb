a4b61a38e4bd347c2796e759dc7baf4c87d7416e
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-11-25 21:31:56 -0500

    API complete, Dtabase is seeded with Faker for Vehicle

--- mend_app_api/db/seeds.rb
+++ mend_app_api/db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+5.times do
+    Mend.create(
+        make: Faker::Vehicle.make,
+        model:Faker::Vehicle.model,
+        year: Faker::Vehicle.year,
+        description: Faker::Vehicle.standard_specs,
+        service: Faker::Subscription.plan 
+    )
+end
