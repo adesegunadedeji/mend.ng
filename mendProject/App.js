e2099f2e80cdab3fc382a671c3fd51fb8399aad1
Adesegun Adedeji <adesegunadedeji@gmail.com>
2019-11-28 11:16:16 -0500

    Fetching from ROR

--- mendProject/App.js
+++ mendProject/App.js
@@ -1,54 +1,48 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-class Greeting extends Component {
+export default  class MendContainer extends Component {
   constructor(){
     super();
     this.state = {
-      isShowingText: true
+      mechanic: []
     }
   }
 
-  
 
 
-  // componentDidMount(){
-  //   setInterval(()=>(
-  //       this.setState(prevState=>(
-  //         { isShowingText: !prevState.isShowingText}
-  //         ))), 1000)
-  //   }
+  componentDidMount(){
+    console.log("Component did Mount");
+    this.getMends();
+}
 
-    componentDidMount(){
-      // Toggle the state every second
-      setInterval(() => (
-        this.setState(previousState => (
-          { isShowingText: !previousState.isShowingText }
-        ))
-      ), 1000);
-    }
-  
+getMends = async()=>{
+  try{
+    const mechanic = await fetch(`http://localhost:3000/mends`,{
+      credentials: "include"
+    })
+    const parsedResponse = await mechanic.json();
+    console.log("*******************************************",parsedResponse);
+    if(parsedResponse.code === 200){
+      this.setState({
+          mechanic: parsedResponse,
+      })
+      console.log("TWST")
+  }
+  }
+  catch(err){
+    console.log(err)
+  }
+}
+
+
 
   render() {
-    if (!this.state.isShowingText) {
-      return null;
-    }
     return (
-      <View>
-        <Text>{this.props.name}</Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+            <Text>This is{this.state.mechanic.make}</Text>
       </View>
     );
   }
 }
 
-export default class LotsOfGreetings extends Component {
-  render() {
-    return (
-      <View style={{alignItems: 'center', top: 50}}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Adesegun' />
-        <Greeting name='Valeera' />
-      </View>
-    );
-  }
-}
